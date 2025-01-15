fc7360f8fcca60f9ce09c04e55b734ff4d7ee1da
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2025-01-15 17:12:07 +0530

    first

--- Automation.ps1
+++ Automation.ps1
@@ -8,5 +8,7 @@ $Global:jsondata = $json
 #### Read Values supplied in JSON file ######
 ### CWA Values ###
 write-host "the json is $json"
+write-host "the json is convert to json before"
 $json | ConvertTo-Json -Depth 10 | Write-Host
+write-host "the json is convert to json after"
 print("hello world")
